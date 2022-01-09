e4de64fc03f949a46e00bb349828473c8578f389
reda hammada <hammada.reda.dev@gmail.com>
2022-01-09 20:04:01 +0100

    update

--- code/app.js
+++ code/app.js
@@ -2,8 +2,11 @@
 function darkTheme(){
 
     let moon = document.getElementById('moon');
+     
+
     if(moon.click){
         document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
+        document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-dark.jpg)";
     }
     
 };
@@ -13,5 +16,6 @@ function lightTheme(){
     let sun = document.getElementById('sun');
     if(sun.click){
         document.body.style.backgroundColor = "white";
+        document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-light.jpg)";
     }
 }
