80c7d46568c53dad516d76f3275ae2116851faaa
reda hammada <hammada.reda.dev@gmail.com>
2022-01-09 20:51:56 +0100

    update

--- code/theme.js
+++ code/theme.js
@@ -1,4 +1,4 @@
-
+// dark theme
 function darkTheme(){
 
     let moon = document.getElementById('moon');
@@ -7,15 +7,39 @@ function darkTheme(){
     if(moon.click){
         document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
         document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-dark.jpg)";
+        removeSun();
     }
     
 };
-
+// light theme
 function lightTheme(){
 
     let sun = document.getElementById('sun');
     if(sun.click){
+
         document.body.style.backgroundColor = "white";
         document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-light.jpg)";
+        removeMoon();
+        
     }
+
 }
+
+// remove the moon 
+function removeSun(){
+
+    let childElem = document.getElementById("moon");
+    childElem.parentNode.removeChild(childElem);
+    
+}
+
+// remove the sun 
+function removeMoon(){
+
+    let childElem = document.getElementById('sun');
+    childElem.parentNode.removeChild(childElem);
+}
+
+
+
+
