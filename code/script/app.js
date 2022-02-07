b12b261b5f964edf526a7e24268e24a6448a5547
reda hammada <hammada.reda.dev@gmail.com>
2022-02-07 23:51:42 +0100

    update

---
+++ code/script/app.js
@@ -0,0 +1,37 @@
+let taskList = [];
+let _readTask = readTask();
+pushArray();
+
+
+
+
+
+
+
+
+
+
+
+
+function readTask(){
+
+    let task = document.getElementById('typeTask').value;
+
+    return task;
+
+}
+
+
+function pushArray(){
+    
+    taskList.push(_readTask);
+
+}
+
+
+function insert(){
+    
+    for(i=0; i<taskList.length; i++){
+        
+    }
+}
--- code/script/theme.js
+++ code/script/theme.js
@@ -6,7 +6,7 @@ function darkTheme(){
 
     if(moon.click){
         document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
-        document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-dark.jpg)";
+        document.querySelector('.todoContainer').style.backgroundImage = "url(../images/bg-desktop-dark.jpg";
         showSun();
         removeMoon();
     }
@@ -19,7 +19,7 @@ function lightTheme(){
     if(sun.click){
 
         document.body.style.backgroundColor = "white";
-        document.querySelector('.todoContainer').style.backgroundImage = "url(/images/bg-desktop-light.jpg)";
+        document.querySelector('.todoContainer').style.backgroundImage = "url(../images/bg-desktop-light.jpg)";
         showMoon();
         removeSun();
         
