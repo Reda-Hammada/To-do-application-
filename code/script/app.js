cca1d08c3198632c819aa2c8e197cbde0946fc2c
reda hammada <hammada.reda.dev@gmail.com>
2022-04-05 22:31:59 +0000

    comments added to javascript code

--- code/script/app.js
+++ code/script/app.js
@@ -1,6 +1,9 @@
+// add a task dynamically 
+
 let taskList = [];
 let check;
 
+// executes all the function below to add the task 
 function addTask(){
 
  tasks = readTask();
@@ -10,19 +13,22 @@ function addTask(){
 
 }
 
+
+// read the value from the input and return it
 function readTask(){
 
     let task = document.getElementById('typeTask').value;
     return task;
 }
 
-
+//  push the value to the array
 function pushArray(){
     
     taskList.push(tasks);
 
 }
 
+// insert the task through looping over the array and insert it into the div
 function insertTask(){  
 
     Elem = document.createElement('div');
@@ -34,38 +40,18 @@ function insertTask(){
         if(task){
 
           taskContainer = document.getElementById('tasksContainer');
-           checkInput =`<input id="check"  type="checkbox">`;
+          checkInput =`<input id="check"  type="checkbox">`;
           Elem.innerHTML = checkInput + taskList[i] ; 
           taskContainer.insertAdjacentElement('beforeend', Elem);
           Elem.classList.add('task');
            
         }
 
-        else {
-
-          if(task.value == null){
-          
-          document.querySelector('#typeTaks:focus').style.outlineColor = red;
-
-        }
         
-        }
+       }
+}
 
-         
-        
-    
-
-          
-       };
-      
-
-     
-    }
-    
-       
-    
-
-
+// reset the input value to empty 
 function reset(){
 
     document.getElementById('typeTask').value = "";
