60744760d273400f1fc3cba575d6e9c4ef3df805
reda hammada <hammada.reda.dev@gmail.com>
2022-04-09 05:20:41 +0000

    update

--- code/script/app.js
+++ code/script/app.js
@@ -4,7 +4,7 @@ const taskList = [];
 let check;
 
 // executes all the function below to add the task 
-function addTask(){
+ addTask = () => {
 
  tasks = readTask();
  pushArray();
@@ -15,14 +15,14 @@ function addTask(){
 
 
 // read the value from the input and return it
-function readTask(){
+ readTask = () =>{
 
     let task = document.getElementById('typeTask').value;
     return task;
 }
 
 //  push the value to the array
-function pushArray(){
+ pushArray = () => {
     
     taskList.push(tasks);
 
@@ -33,47 +33,66 @@ function pushArray(){
 count = () => {
 
     for(i=0; i<taskList.length; i++){
-
         return taskList.length;
     }
 }
+
+
+
+// create commands container 
+createCommands = () => {
+  
+     commandContainer = document.getElementById('commandsContainer');
+     commands = document.createElement('div');
+     commands.innerHTML = `<p>${count()} items left</p>` + `<ul style =display:inline;><li>All</li>Active<li>Completed</li></ul>` + `<p>Clear Completed</p>`;
+     commandContainer.insertAdjacentElement('beforeend', commands);
+     commands.classList.add('commandContainer');
+}
+
 // insert the task through looping over the array and insert it into the div
-function insertTask(){  
-
-    count();
-     
-
-    
+ insertTask = () =>{  
+ 
     Elem = document.createElement('div');
     
      let task = document.getElementById('typeTask').value;
 
     for(i=0; i < taskList.length; i++){
+    
 
-        if(task){
-
+        if(task) {
+ 
           taskContainer = document.getElementById('tasksContainer');// task container 
           checkInput =`<input id="check"  type="checkbox">`;//  content of task container div
-          Elem.innerHTML = checkInput + taskList[i] ; 
+          Elem.innerHTML = checkInput + taskList[i] + `<div class="delete">x</div>` ; 
           taskContainer.insertAdjacentElement('beforeend', Elem);
           Elem.classList.add('task');  //adding style to task container content 
-          
-          
-          
+          createCommands();
+
+          while(commandContainer.hasChildNodes ){
+
+                commandContainer.removeChild(commandContainer.firstElementChild);
+          }
+
         }
- 
+
+        
+
+        } 
+    }
+
+        
+
 
 
 
         
-    }
-}
+    
 
 
 
 // reset the input value to empty 
-function reset(){
+ reset = () => {
 
     document.getElementById('typeTask').value = "";
-};
+}
 
